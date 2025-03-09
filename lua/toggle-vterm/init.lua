99697f238f262df81657c71db83e518bd8c08dd9
Christian Benitez <cbenitezdiaz@gmail.com>
2025-03-09 12:54:54 -0300

    Fix terminal close on exit using autocmd

--- lua/toggle-vterm/init.lua
+++ lua/toggle-vterm/init.lua
@@ -18,11 +18,11 @@ function M.toggle_terminal(orientation)
 		if term.buf and vim.api.nvim_buf_is_valid(term.buf) then
 			-- Reuse the existing terminal buffer
 			if orientation == "horizontal" then
-				vim.cmd("split")                     -- Open a horizontal split
+				vim.cmd("split") -- Open a horizontal split
 			else
-				vim.cmd("vsplit")                    -- Open a vertical split
+				vim.cmd("vsplit") -- Open a vertical split
 			end
-			vim.api.nvim_win_set_buf(0, term.buf)  -- Set the terminal buffer in the new window
+			vim.api.nvim_win_set_buf(0, term.buf) -- Set the terminal buffer in the new window
 			term.win = vim.api.nvim_get_current_win() -- Save the terminal window
 		else
 			-- If there is no terminal buffer, create a new one
@@ -31,18 +31,27 @@ function M.toggle_terminal(orientation)
 			else
 				vim.cmd("vsplit | terminal")
 			end
-			term.buf = vim.api.nvim_get_current_buf()              -- Save the terminal buffer
-			term.win = vim.api.nvim_get_current_win()              -- Save the terminal window
+			term.buf = vim.api.nvim_get_current_buf() -- Save the terminal buffer
+			term.win = vim.api.nvim_get_current_win() -- Save the terminal window
 			vim.api.nvim_buf_set_option(term.buf, "buflisted", false) -- Mark the terminal buffer as non-listable
 
 			-- Set keybinding for Ctrl+L to clear the terminal
 			vim.api.nvim_buf_set_keymap(term.buf, "t", "<C-l>", "<Cmd>clear<CR>", { noremap = true, silent = true })
 
 			-- Automatically close the terminal when the shell exits
-			vim.api.nvim_buf_set_option(term.buf, "term_close_on_exit", true)
+			vim.api.nvim_create_autocmd("TermClose", {
+				buffer = term.buf,
+				callback = function()
+					if vim.api.nvim_win_is_valid(term.win) then
+						vim.api.nvim_win_close(term.win, true)
+					end
+					term.buf = nil
+					term.win = nil
+				end,
+			})
 		end
-		vim.cmd("startinsert")      -- Enter Insert mode when opening the terminal
-		vim.wo.number = false       -- Disable line numbers
+		vim.cmd("startinsert") -- Enter Insert mode when opening the terminal
+		vim.wo.number = false -- Disable line numbers
 		vim.wo.relativenumber = false -- Disable relative line numbers
 	end
 end
