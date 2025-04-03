110025d33fff1ca854d982d1a1dec0100c541a54
Christian Benitez <cbenitezdiaz@gmail.com>
2025-04-03 17:05:13 -0300

    fix terminal dimensions when changing windows

--- lua/toggle-vterm/init.lua
+++ lua/toggle-vterm/init.lua
@@ -1,16 +1,22 @@
 local M = {}
-
 -- Variables to track the terminal states
 local terminals = {
-	horizontal = { buf = nil, win = nil },
-	vertical = { buf = nil, win = nil },
+	horizontal = { buf = nil, win = nil, height = nil },
+	vertical = { buf = nil, win = nil, width = nil },
 }
 
 -- Function to toggle a terminal in a specific orientation (horizontal or vertical)
 function M.toggle_terminal(orientation)
 	local term = terminals[orientation]
+
 	if term.win and vim.api.nvim_win_is_valid(term.win) then
-		-- If the terminal window is already open, hide it
+		-- Store current window dimensions before hiding
+		if orientation == "horizontal" then
+			term.height = vim.api.nvim_win_get_height(term.win)
+		else
+			term.width = vim.api.nvim_win_get_width(term.win)
+		end
+		-- Hide the terminal window
 		vim.api.nvim_win_hide(term.win)
 		term.win = nil
 	else
@@ -24,6 +30,13 @@ function M.toggle_terminal(orientation)
 			end
 			vim.api.nvim_win_set_buf(0, term.buf)  -- Set the terminal buffer in the new window
 			term.win = vim.api.nvim_get_current_win() -- Save the terminal window
+
+			-- Restore the previous dimensions if they exist
+			if orientation == "horizontal" and term.height then
+				vim.api.nvim_win_set_height(term.win, term.height)
+			elseif orientation == "vertical" and term.width then
+				vim.api.nvim_win_set_width(term.win, term.width)
+			end
 		else
 			-- If there is no terminal buffer, create a new one
 			if orientation == "horizontal" then
@@ -34,6 +47,13 @@ function M.toggle_terminal(orientation)
 			term.buf = vim.api.nvim_get_current_buf()              -- Save the terminal buffer
 			term.win = vim.api.nvim_get_current_win()              -- Save the terminal window
 			vim.api.nvim_buf_set_option(term.buf, "buflisted", false) -- Mark the terminal buffer as non-listable
+
+			-- Set default dimensions (opcional)
+			if orientation == "horizontal" then
+				term.height = vim.api.nvim_win_get_height(term.win)
+			else
+				term.width = vim.api.nvim_win_get_width(term.win)
+			end
 
 			-- Set keybinding for Ctrl+L to clear the terminal
 			vim.api.nvim_buf_set_keymap(term.buf, "t", "<C-l>", "clear<CR>", { noremap = true, silent = true })
@@ -47,6 +67,12 @@ function M.toggle_terminal(orientation)
 					end
 					term.buf = nil
 					term.win = nil
+					-- Reset size as well when terminal is closed
+					if orientation == "horizontal" then
+						term.height = nil
+					else
+						term.width = nil
+					end
 				end,
 			})
 		end
@@ -54,6 +80,28 @@ function M.toggle_terminal(orientation)
 		vim.wo.number = false       -- Disable line numbers
 		vim.wo.relativenumber = false -- Disable relative line numbers
 	end
+end
+
+-- Add a function to manually save current terminal dimensions
+function M.save_terminal_dimensions()
+	for orientation, term in pairs(terminals) do
+		if term.win and vim.api.nvim_win_is_valid(term.win) then
+			if orientation == "horizontal" then
+				term.height = vim.api.nvim_win_get_height(term.win)
+			else
+				term.width = vim.api.nvim_win_get_width(term.win)
+			end
+		end
+	end
+end
+
+-- Add auto-command to save terminal dimensions when changing windows
+function M.setup()
+	vim.api.nvim_create_autocmd("WinLeave", {
+		callback = function()
+			M.save_terminal_dimensions()
+		end,
+	})
 end
 
 return M
